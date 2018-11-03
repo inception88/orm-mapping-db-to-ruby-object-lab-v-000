a82668dd7be088e5a050169b262ddbefa2fef999
Benjamin Jones <bljones36@gmail.com>
2018-11-03 20:43:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,4 +83,17 @@ class Student
         self.new_from_db(row)
       end
     end
+
+    def self.first_X_students_in_grade_10
+      sql = <<-SQL
+         SELECT *
+         FROM students
+         GROUP BY name
+         HAVING grade < 12
+       SQL
+
+       DB[:conn].execute(sql).collect do |row|
+         self.new_from_db(row)
+       end
+     end
 end
